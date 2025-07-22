fb4c893cf9e453033acfbd89c68d12936723b81a
UcielD89 <ucielprogramador21@gmail.com>
2025-07-22 00:41:55 -0300

    feature: Firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,29 @@
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+
+const API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
+const AUTH_DOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const PROJECT_ID = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const STORE_BUCKET = import.meta.env.VITE_FIREBASE_STORE_BUCKET;
+const MESSAGING_SENDER_ID = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const APP_ID = import.meta.env.VITE_FIREBASE_APP_ID;
+const MEASUREMENT_ID = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Exportar Firestore
+export const db = getFirestore(app);
